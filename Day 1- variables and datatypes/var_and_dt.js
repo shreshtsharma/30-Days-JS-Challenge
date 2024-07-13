27a3020c3491a0170756896133eb893e69c65736
shreshtsharma <sharmashresht7@gmail.com>
2024-07-13 23:19:51 +0530

    DAY-1 CHALLENGE

---
+++ Day 1- variables and datatypes/var_and_dt.js
@@ -0,0 +1,39 @@
+var num=10;
+console.log(num);
+
+let name="shresht"
+console.log(name);
+
+const result=true;
+console.log(result);
+
+// Datatypes
+let a=10;
+const username="sharma";
+const flag=true;
+
+let user={
+    name:"shresht",
+    age:"21",
+    isLoggedIn:"false"
+};
+
+let arr=[1,23,4,5];
+
+console.log(typeof(a));
+console.log(typeof(username));
+console.log(typeof(flag));
+console.log(typeof(user));
+console.log(typeof(arr));
+
+let balance=10000;
+console.log(balance);
+balance=20000;
+console.log(balance);
+
+const bal=1000000
+console.log(bal);
+// reassigning a const variable
+bal=20000
+console.log(bal);
+// TypeError: Assignment to constant variable.
