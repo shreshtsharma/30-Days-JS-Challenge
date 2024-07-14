8f2f62e32b8691114aaf14a83e35610f8d653ac6
shreshtsharma <sharmashresht7@gmail.com>
2024-07-14 22:53:34 +0530

    DAY-2

---
+++ Day 2 Operators/operators.js
@@ -0,0 +1,34 @@
+let a=10 , b=20;
+
+
+// Arithmetic Operators
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+a+=30;
+console.log(a);
+b-=10;
+console.log(b);
+
+// Comparison Operators
+console.log(a>b);
+console.log(b>a);
+
+console.log(10>=20);
+console.log(10<=20);
+
+let num=4;
+let num2="4"
+console.log(num==num2);
+console.log(num==="num2");
+
+// Logical Operators
+console.log(10<20 && 10>=9);
+console.log(10>20 || 10>=9);
+console.log(!(10>20));
+
+// Ternary Operators
+num>0?console.log("positive"):console.log("negative");
